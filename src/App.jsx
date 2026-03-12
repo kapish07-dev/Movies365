2f1c99cd62abc187c72e25f08013aef223188675
kapish07-dev <kapishbarhate7@gmail.com>
2026-03-12 23:33:08 +0530

    app setup done
    :)

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+// services/api.jsx is optional
+
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import MovieDetails from "./pages/MovieDetails";
+import FavoritesPage from "./pages/FavoritesPage";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      {/* Header with navigation */}
+      <header className="app-header">
+        <div className="container header-inner">
+          <Link to="/" className="logo">
+            🎬 KapixxMovies.io
+          </Link>
+          <nav className="header-nav">
+            <Link className="up-effect" to="/">Home</Link>
+            <Link className="up-effect" to="/favorites">Favorites</Link>
+          </nav>
+        </div>
+      </header>
+
+      {/* Main routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+
+      {/* Footer */}
+      <footer className="app-footer">
+        <div className="container">
+          <p>Made with 🧠 using React + OMDB API</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
