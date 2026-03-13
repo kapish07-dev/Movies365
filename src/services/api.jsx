a2b85679930b942ab89e0f0c6879def68a83cf2a
kapish07-dev <kapishbarhate7@gmail.com>
2026-03-13 10:27:49 +0530

    api add
    API services added

---
+++ src/services/api.jsx
@@ -0,0 +1,21 @@
+// const API_KEY = "7dfbc4ab"; // OMDB key
+const API_KEY = "4a3b711b"; 
+const BASE_URL = "https://www.omdbapi.com/";
+
+export async function fetchMovies(query) {
+    if (!query) return [];
+
+    try {
+        const res = await fetch(`${BASE_URL}?apikey=${API_KEY}&s=${query}`);
+        const data = await res.json();
+
+        if (data.Response === "True") {
+            return data.Search;
+        } else {
+            return [];
+        }
+    } catch (error) {
+        console.error("Error fetching movies:", error);
+        return [];
+    }
+}
