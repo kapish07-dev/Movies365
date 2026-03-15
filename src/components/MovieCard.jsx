e7ebe2b4bf911191f9cfa86e16d70998c19906f6
kapish07-dev <kapishbarhate7@gmail.com>
2026-03-15 18:36:11 +0530

    Update MovieCard.jsx
    CSS enhanced

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -30,7 +30,56 @@ export default function MovieCard({ movie, addToFavorites }) {
         />
       </Link>
 
-      <div className="movie-info" style={{ padding: 8 }}>
+      <div
+        className="movie-info"
+        style={{
+          padding: 8,
+          display: "flex",
+          flexDirection: "column",
+          height: "100%",
+        }}
+      >
+        <h3 style={{ fontSize: 15, margin: "0 0 2px" }}>{movie.Title}</h3>
+        <p style={{ margin: 0, color: "var(--muted)", fontSize: 13 }}>
+          {movie.Year}
+        </p>
+        <div
+          style={{
+            marginTop: "auto",
+            display: "flex",
+            gap: 8,
+          }}
+        >
+          <button
+            className="up-effect"
+            onClick={() => addToFavorites(movie)}
+            style={{
+              padding: "4px 10px 8px 10px",
+              marginTop: "8px",
+              background: "transparent",
+              border: "1px solid rgba(255,255,255,0.06)",
+              color: "#fff",
+              borderRadius: 8,
+              fontWeight: 600,
+            }}
+          >
+            ⭐ Favorite
+          </button>
+
+          <Link
+            to={`/movie/${movie.imdbID}`}
+            className="up-effect"
+            style={{
+              marginLeft: "auto",
+              alignSelf: "center",
+              color: "var(--accent)",
+            }}
+          >
+            Details →
+          </Link>
+        </div>
+      </div>
+      {/* <div className="movie-info" style={{ padding: 8 }}>
         <h3 style={{ fontSize: 15, margin: "0 0 2px" }}>{movie.Title}</h3>
         <p style={{ margin: 0, color: "var(--muted)", fontSize: 13 }}>
           {movie.Year}
@@ -62,7 +111,7 @@ export default function MovieCard({ movie, addToFavorites }) {
             Details →
           </Link>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
